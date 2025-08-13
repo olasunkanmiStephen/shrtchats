c346c416ed4bf813df503f4b542f20172eac06a9
Stephenolasunkanmi <olasunkanmistephen45@gmail.com>
2025-08-13 14:34:24 +0100

    fix error in message contoller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,20 +1,21 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js"
 
-export const getUsersForSidebar = async () => {
+export const getUsersForSidebar = async (req, res) => {
     try {
-        const loggedInUserId = requestAnimationFrame.user._id;
+        const loggedInUserId = req.user._id;
 
         const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        resizeBy.status(200).json(filteredUsers);
+        res.status(200).json(filteredUsers);
     } catch (error) {
         console.log("Error in getUsersForSidebar: ", error.message);
         resizeBy.status(500).json({ error: "Internal server error" })
     }
 }
 
-export const getMessage = async () => {
+export const getMessage = async (req, res) => {
     try {
         const { id: userToChatId } = req.params;
         const myId = req.user._id;
@@ -25,12 +26,16 @@ export const getMessage = async () => {
                 { senderId: userToChatId, receiverId: myId },
             ]
         })
+
+        res.status(200).json(messages);
     } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
         
     }
 }
 
-export const sendMessage = async () => {
+export const sendMessage = async (req, res) => {
     try {
         const { text, image} = req.body;
         const { id: receiverId } = req.params;
