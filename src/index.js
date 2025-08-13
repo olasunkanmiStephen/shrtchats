7b87ee6019b7c568ed128d83d1bf45c837c63004
Stephenolasunkanmi <olasunkanmistephen45@gmail.com>
2025-08-13 12:35:51 +0100

    update profile controller and cloudinary for profilr picture upload configured

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // parses incoming JSON
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
